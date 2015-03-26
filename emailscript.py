72639a9d73787eaf7ef731de47a7b596cdad3523
Harrison <Harrison@sentdex.com>
2015-03-25 18:33:56 -0700

    Update emailscript.py
    added brief explanation.

--- emailscript.py
+++ emailscript.py
@@ -1,5 +1,9 @@
-import smtplib
+'''
+This script is just used to send me an alert if there has been a donation. Since people are allowed to request things via donations,
+I need to check the email to see if there were any requests. Just a simple email to do this.
+'''
 
+import smtplib
 
 msg = 'Got a donation success, check.'
 server = smtplib.SMTP('smtp.gmail.com',587) #port 587 or 465
